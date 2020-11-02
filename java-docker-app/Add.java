1aedd4d3af4785979fbf42278a32244f069bbb64
Venkata Sai Hitesh Jonnalagadda <saihitesh98@gmail.com>
2020-11-02 18:55:33 +0900

    feat: cmd and args in addition

--- java-docker-app/Add.java
+++ java-docker-app/Add.java
@@ -1,10 +1,7 @@
-public class Add {
-
-    public static void main(String[] args) {
-         
-       int num1 = 5, num2 = 15, sum;
-       sum = num1 + num2;
- 
-       System.out.println("Sum of 15 and 5 is: "+sum);
-    }
- }
+public class Add
+{
+   public static void main(String[] args)
+   {
+   System.out.println("Addition of two numbers " + args[0] + " + " + args[1] + " is " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
+   }
+}
